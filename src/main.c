c944e9cb515864e83c6cffe9bd52530455d19e63
Theo Chartier <tchartie@1A1.42angouleme.fr>
2024-02-08 16:13:03 +0100

    Add free

--- src/main.c
+++ src/main.c
@@ -14,6 +14,48 @@
 #include "../include/libft.h"
 #include "../include/ft_printf.h"
 
+static void	destroy_texture(t_map *map)
+{
+	mlx_delete_texture(map->tx->floor[0]);
+	mlx_delete_texture(map->tx->floor[1]);
+	mlx_delete_texture(map->tx->floor[2]);
+	mlx_delete_texture(map->tx->player);
+	mlx_delete_texture(map->tx->wall[0]);
+	mlx_delete_texture(map->tx->wall[1]);
+	mlx_delete_texture(map->tx->wall[2]);
+	mlx_delete_texture(map->tx->wall[3]);
+	mlx_delete_texture(map->tx->corner[0]);
+	mlx_delete_texture(map->tx->corner[1]);
+	mlx_delete_texture(map->tx->corner[2]);
+	mlx_delete_texture(map->tx->corner[3]);
+	mlx_delete_texture(map->tx->obstacle[0]);
+	mlx_delete_texture(map->tx->collectible[0]);
+	mlx_delete_texture(map->tx->collectible[1]);
+	mlx_delete_texture(map->tx->exit[0]);
+	mlx_delete_texture(map->tx->exit[1]);
+}
+
+static void	destroy_image(t_map *map)
+{
+	mlx_delete_image(map->mlx, map->img->floor[0]);
+	mlx_delete_image(map->mlx, map->img->floor[1]);
+	mlx_delete_image(map->mlx, map->img->floor[2]);
+	mlx_delete_image(map->mlx, map->img->player);
+	mlx_delete_image(map->mlx, map->img->wall[0]);
+	mlx_delete_image(map->mlx, map->img->wall[1]);
+	mlx_delete_image(map->mlx, map->img->wall[2]);
+	mlx_delete_image(map->mlx, map->img->wall[3]);
+	mlx_delete_image(map->mlx, map->img->corner[0]);
+	mlx_delete_image(map->mlx, map->img->corner[1]);
+	mlx_delete_image(map->mlx, map->img->corner[2]);
+	mlx_delete_image(map->mlx, map->img->corner[3]);
+	mlx_delete_image(map->mlx, map->img->obstacle[0]);
+	mlx_delete_image(map->mlx, map->img->collectible[0]);
+	mlx_delete_image(map->mlx, map->img->collectible[1]);
+	mlx_delete_image(map->mlx, map->img->exit[0]);
+	mlx_delete_image(map->mlx, map->img->exit[1]);
+}
+
 static char	*create_arg(char *argv)
 {
 	char	*arg;
@@ -44,7 +86,12 @@ int	main(int argc, char **argv)
 		map_init(&map, arg);
 		player_init(&map, &player);
 		check_path(&map, &player, arg);
+		free(arg);
 		game_init(map, player);
-		//free()
+		destroy_image(map);
+		destroy_texture(map);
+		mlx_terminate(map->mlx);
+		free_struct_map(map);
+		free(player);
 	}
 }
